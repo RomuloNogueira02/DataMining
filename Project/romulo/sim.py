25ec81bf73d61eeb7989c61b724ac2b02c69ebdb
Rómulo <romulo3218@gmail.com>
2024-06-04 14:34:24 +0100

    maybe something cool :D

---
+++ Project/romulo/sim.py
@@ -0,0 +1,33 @@
+import pickle
+from datasketch import MinHash, MinHashLSH
+from nltk.metrics import jaccard_distance
+
+
+with open("../mol_bits.pkl", "rb") as f:
+    MOL_BITS = pickle.load(f)
+
+LSH = MinHashLSH(threshold=0.5, num_perm=256)
+
+# Função para criar MinHash a partir de uma lista de valores
+def create_minhash(values):
+    m = MinHash(num_perm=256)
+    for val in values:
+        m.update(str(val).encode('utf8'))
+    return m
+
+for key, values in MOL_BITS.items():
+    m = create_minhash(values)
+    LSH.insert(key, m)
+
+
+def find_similar_keys(target_key, threshold=0.5):
+    target_minhash = create_minhash(MOL_BITS[target_key])
+    result = LSH.query(target_minhash)
+    toReturn = []
+    for key in result:
+        score = 1 - jaccard_distance(set(MOL_BITS[target_key]), set(MOL_BITS[key]))
+        if score > threshold:
+            toReturn.append({(target_key, key): score})
+
+    return sorted(toReturn, key=lambda x: list(x.values())[0], reverse=True)
+
