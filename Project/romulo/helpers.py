5a1edbfe5948b560fb9d72f9aa5727e450de290f
Rómulo <romulo3218@gmail.com>
2024-06-05 16:34:13 +0100

    hybrid approach

--- Project/romulo/helpers.py
+++ Project/romulo/helpers.py
@@ -2,4 +2,4 @@ def remove_blank_space(string):
     return string.replace(" ", "")
 
 def diff_percent(value1, value2):
-    return round(abs((value1 - value2) / value1) * 100)
+    return abs(round((1 - value1 / value2) * 100, 1))
